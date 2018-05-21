25ec8c8cde4067098512099c5e99a27ac4262d28
Phạm Phong Phú <phuphamphong97@gmail.com>
2018-05-21 10:41:49 +0700

    Kết nối CSDL

---
+++ QuanLyKhachSan/GUI/tesst.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DTO;
+using DAL1;
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for tesst.xaml
+    /// </summary>
+    public partial class tesst : Window
+    {
+        NhanVienDAL bl = new NhanVienDAL();
+        public tesst()
+        {
+            InitializeComponent();
+            Employee_View_Load();
+        }
+        private void Employee_View_Load()
+        {
+            dt.AutoGenerateColumns = false;
+           
+            
+            dt.ItemsSource = bl.layNhanVien(); 
+            
+        }
+    }
+}
