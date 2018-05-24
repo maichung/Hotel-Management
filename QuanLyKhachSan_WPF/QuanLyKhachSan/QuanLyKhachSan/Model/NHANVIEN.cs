97a65669f1b728a52334952e72f68a781392e19d
BuiHuyPhuc <buihuyphuc97@gmail.com>
2018-05-24 22:04:00 +0700

    finally WPF demo

--- QuanLyKhachSan_WPF/QuanLyKhachSan/QuanLyKhachSan/Model/NHANVIEN.cs
+++ QuanLyKhachSan_WPF/QuanLyKhachSan/QuanLyKhachSan/Model/NHANVIEN.cs
@@ -9,26 +9,36 @@
 
 namespace QuanLyKhachSan.Model
 {
+    using QuanLyKhachSan.ViewModel;
     using System;
     using System.Collections.Generic;
     
-    public partial class NHANVIEN
+    public partial class NHANVIEN : BaseViewModel
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public NHANVIEN()
         {
             this.HOADONs = new HashSet<HOADON>();
         }
-    
-        public int MA_NV { get; set; }
-        public int MA_TK { get; set; }
-        public string HOTEN_NV { get; set; }
-        public Nullable<bool> GIOITINH_NV { get; set; }
-        public Nullable<System.DateTime> NGAYSINH_NV { get; set; }
-        public string SODIENTHOAI_NV { get; set; }
-        public string CHUCVU_NV { get; set; }
-        public string DIACHI_NV { get; set; }
-        public Nullable<System.DateTime> NGAYVAOLAM_NV { get; set; }
+
+        private int _MA_NV;
+        public int MA_NV { get => _MA_NV; set { _MA_NV = value; OnPropertyChanged(); } }
+        private int _MA_TK;
+        public int MA_TK { get => _MA_TK; set { _MA_TK = value; OnPropertyChanged(); } }
+        private string _HOTEN_NV;
+        public string HOTEN_NV { get => _HOTEN_NV; set { _HOTEN_NV = value; OnPropertyChanged(); } }
+        private Nullable<bool> _GIOITINH_NV;
+        public Nullable<bool> GIOITINH_NV { get => _GIOITINH_NV; set { _GIOITINH_NV = value; OnPropertyChanged(); } }
+        private Nullable<System.DateTime> _NGAYSINH_NV;
+        public Nullable<System.DateTime> NGAYSINH_NV { get => _NGAYSINH_NV; set { _NGAYSINH_NV = value; OnPropertyChanged(); } }
+        private string _SODIENTHOAI_NV;
+        public string SODIENTHOAI_NV { get => _SODIENTHOAI_NV; set { _SODIENTHOAI_NV = value; OnPropertyChanged(); } }
+        private string _CHUCVU_NV;
+        public string CHUCVU_NV { get => _CHUCVU_NV; set { _CHUCVU_NV = value; OnPropertyChanged(); } }
+        private string _DIACHI_NV;
+        public string DIACHI_NV { get => _DIACHI_NV; set { _DIACHI_NV = value; OnPropertyChanged(); } }
+        private Nullable<System.DateTime> _NGAYVAOLAM_NV;
+        public Nullable<System.DateTime> NGAYVAOLAM_NV { get => _NGAYVAOLAM_NV; set { _NGAYVAOLAM_NV = value; OnPropertyChanged(); } }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<HOADON> HOADONs { get; set; }
---
+++ QuanLyKhachSan_WPF/QuanLyKhachSan/QuanLyKhachSan/Model/ThongTinNhanVien.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhachSan.Model
+{
+    public class ThongTinNhanVien
+    {
+        public TAIKHOAN TaiKhoan { get; set; }
+        public NHANVIEN NhanVien { get; set; }
+    }
+}
